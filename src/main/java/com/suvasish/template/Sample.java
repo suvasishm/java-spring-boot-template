490249ebddab3fc548342b1afd9c3e32c1814dbf
Suvasish Mondal <suvasishmndl@gmail.com>
2017-04-05 18:33:22 +0530

    - Class Template

---
+++ src/main/java/com/suvasish/template/Sample.java
@@ -0,0 +1,20 @@
+package com.suvasish.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by suvasish on 05-Apr-17.
+ */
+@SpringBootApplication
+public class Sample {
+    public static void main(String[] args) {
+        SpringApplication.run(Sample.class, args);
+
+
+        System.out.println("Hello, World!");
+
+
+        System.exit(0);
+    }
+}
